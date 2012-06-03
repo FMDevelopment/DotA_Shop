1d4443a2d638191a7fd0273edd825d1d7435dc87
Maximilian Jacobs <mad@fmstudio.de>
2012-06-03 21:19:01 +0200

    Version 0.1.1

--- src/main/java/de/fmstudio/dev/dota_shop/DotA_Shop.java
+++ src/main/java/de/fmstudio/dev/dota_shop/DotA_Shop.java
@@ -8,44 +8,46 @@ package de.fmstudio.dev.dota_shop;
  *
  * @author Max
  */ 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import com.esotericsoftware.yamlbeans.*;
+import de.fmstudio.dev.dota_shop.micronomy.AccountManager;
+import java.util.logging.*;
 import java.io.*;
 import java.util.*;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import com.orange451.UltimateArena.*;
 public class DotA_Shop extends JavaPlugin {
     public Logger out;
     private String arena;
+    private File config = new File("plugins/DotaShop","config.yml");
     @Override
     public void onEnable(){
         out = this.getLogger();
-        out.info("DotA Shop for Ultimate Arena 0.0.1 Enabled");
-        boolean config = (new File("plugins/DotaShop","config.yml").exists());
-        if(config)
+        out.info("DotA Shop for Ultimate Arena 0.1.1 Enabled");
+        boolean checkconf = (config.exists());
+        if(checkconf)
         {
             try{
-                FileInputStream in = new FileInputStream(new File("plugins/DotaShop","config.yml"));
-                Properties prop = new Properties();
-                prop.load(in);
-                in.close();
+                YamlReader in = new YamlReader(new FileReader(config));
+                Map conf = (Map)in.read();
+                arena = conf.get("arena").toString();
                 out.info("DotA Shop Config Loaded");
-                arena = prop.getProperty("arena");
             }
-            catch(Exception e){
-              
-            }
+            catch(Exception e){}
         }
         else{
             File f = new File("plugins/DotaShop","config.yml");
             try {
                 f.createNewFile();
+                BufferedWriter write = new BufferedWriter(new FileWriter(f));
+                write.write("arena: none");
+                write.newLine();
+                write.append("initial-amount: 600");
+                write.close();
                 out.info("Config File successfully created");
                 out.info("Please specify your DotA Arena with '/dshop arena [arena name]'");
-            } catch (IOException ex) {
-                
-            }
+            } catch (IOException ex) {}
         }
         
 }
@@ -53,7 +55,7 @@ public class DotA_Shop extends JavaPlugin {
     
     @Override
     public void onDisable(){
-        out.info("Dota Shop for Ultimate Arena 0.0.1 Disabled");
+        out.info("Dota Shop for Ultimate Arena 0.1.1 Disabled");
     }
     
     @Override
@@ -61,9 +63,28 @@ public class DotA_Shop extends JavaPlugin {
         Player player = null;
         if(sender instanceof Player){
         player = (Player) sender;
+    if(args.length==2){
+        if(cmd.getName().equalsIgnoreCase("dota")&&(args[1].equalsIgnoreCase("j")||args[1].equalsIgnoreCase("join"))){ 
+            new AccountManager().start();
+        }
     }
-        if(cmd.getName().equalsIgnoreCase("ua")&&(args[1].equalsIgnoreCase("j")||args[1].equalsIgnoreCase("join"))&&args[2].equalsIgnoreCase(arena)){
-            
+        if(cmd.getName().equals("dota")&&args[1].equalsIgnoreCase("arena")){
+            try {
+                arena = args[2];
+                YamlReader in = new YamlReader(new FileReader(config));
+                Map conf = (Map)in.read();
+                String save = conf.get("initial-value").toString();
+                conf.put("arena", args[2]);
+                conf.put("initial-value", save);
+                YamlWriter writer = new YamlWriter(new FileWriter(config));
+                writer.write(conf);
+                writer.close();
+            } 
+            catch (IOException ex){return false;}
+        }
+    } else {
+            sender.sendMessage("You must be a Player!");
+            return false;
         }
         return false;
     }
